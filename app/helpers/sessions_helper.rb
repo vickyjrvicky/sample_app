860328ef2c799ed2b0716075f6e24009a9fe9fcc
Vignesh Janakiramar <vignesh.jk@freshdesk.com>
2015-03-10 16:52:24 +0530

    Finish update in the middle

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,6 @@ module SessionsHelper
   	self.current_user = user
   end
 
-  def sign_out
-  	self.current_user = nil
-  	cookies.delete(:remember_token)
-  end
-
   def current_user=(user)
     @current_user = user
   end
@@ -18,7 +13,16 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
   	!current_user.nil?
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
